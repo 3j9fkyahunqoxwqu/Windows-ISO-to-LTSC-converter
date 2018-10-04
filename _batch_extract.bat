767846784c68ceced755840d410dc315110c6842
CHEF-KOCH <CHEF-KOCH@protonmail.com>
2018-10-04 14:44:44 +0200

    v1.0

---
+++ _batch_extract.bat
@@ -0,0 +1,28 @@
+@echo off
+
+
+for /r %%i in (*.svf) do _smv.exe x "%%~nxi" -br .
+
+  echo -------------------------------------------------------------------------------
+)
+
+
+
+
+
+
+@echo off
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo ------------FiNiSHED------------
+echo.
+echo.
+echo.
+echo.
+pause
+
